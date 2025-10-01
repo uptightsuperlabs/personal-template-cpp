1fd814073188b134080f76c921d3db44c2408455
uptightsuperlabs <uptight283@gmail.com>
2025-10-01 04:54:13 -0400

    cleanup

--- backend/main.cpp
+++ backend/main.cpp
@@ -1,18 +1,5 @@
 #include <windows.h>
-#include <iostream>
-#include <thread>
-
-bool is_running = false;
-
-void cheat_attach_thread(HINSTANCE instance) {
-    std::cout << "attached @ 0x" << std::hex << &instance << std::endl;
-}
 
 BOOL APIENTRY DllMain(HINSTANCE instance, DWORD reason, LPVOID reserved) {
-    if (DLL_PROCESS_ATTACH && !is_running) {
-        std::thread(cheat_attach_thread, instance).detach();
-        is_running = true;
-    }
-
     return TRUE;
 }
