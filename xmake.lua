7c48f5600c90c12c7db85fc68a3b5462cdbd12bd
uptightsuperlabs <uptight283@gmail.com>
2025-10-31 05:43:57 +0000

    add binary version

--- xmake.lua
+++ xmake.lua
@@ -4,10 +4,6 @@ local packages = {};
 local build_name = "disco";
 local build_ver = "c++17";
 
-local is_debug = (
-    is_mode("debug") or is_mode("releasedbg") and true
-) or false;
-
 do
     add_rules("plugin.compile_commands.autoupdate", {
         outputdir = ".vscode"
@@ -29,16 +25,9 @@ do
     set_languages(build_ver);
 
     target("frontend") do
-        set_basename(is_debug and ("frontend") or build_name);
+        set_basename(build_name);
         set_kind("binary");
         add_files("frontend/*.cpp", "assets/placeholder.rc");
-        add_packages(table.unpack(_packages));
-    end
-
-    target("backend") do
-        set_basename(is_debug and ("backend") or build_name);
-        set_kind("shared");
-        add_files("backend/*.cpp");
         add_packages(table.unpack(_packages));
     end
 end
